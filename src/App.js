b457607725eace8d2cd6be3bd2657142c10b21dc
Ben Yang <benayang@gmail.com>
2018-10-10 16:46:17 -0700

    wait, nm, that breaks everything

--- src/App.js
+++ src/App.js
@@ -72,7 +72,6 @@ class App extends Component {
 
   componentWillMount() {
     window.addEventListener('resize', () => this.setState({compact: window.innerWidth <= compactWidth}));
-    window.onpopstate = e => window.location.reload();
   }
 
   componentDidMount() {
